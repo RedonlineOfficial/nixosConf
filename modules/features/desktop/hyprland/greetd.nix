143528f66d04baf2791c61ec8871b9f230f56d6f
RedonlineOfficial <dev@redonline.me>
2026-04-15 07:40:34 -0700

    fix: use lib.mkForce for regreet dark theme to override Stylix default

--- modules/features/desktop/hyprland/greetd.nix
+++ modules/features/desktop/hyprland/greetd.nix
@@ -1,10 +1,10 @@
 { self, inputs, ... }: {
 
-  flake.nixosModules.greetd = { ... }: {
+  flake.nixosModules.greetd = { lib, ... }: {
 
     programs.regreet = {
       enable = true;
-      settings.GTK.application_prefer_dark_theme = true;
+      settings.GTK.application_prefer_dark_theme = lib.mkForce true;
     };
 
   };
