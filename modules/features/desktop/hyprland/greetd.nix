15af46235c020a7e1b6b5a434ecf521f9436cd09
RedonlineOfficial <dev@redonline.me>
2026-04-15 07:23:54 -0700

    fix: use renamed tuigreet package path

--- modules/features/desktop/hyprland/greetd.nix
+++ modules/features/desktop/hyprland/greetd.nix
@@ -6,7 +6,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --cmd start-hyprland";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --remember --cmd start-hyprland";
           user = "greeter";
         };
       };
