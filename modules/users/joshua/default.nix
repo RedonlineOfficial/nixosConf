1c8aaf64ae954a0feb4db7d75dff2c7c155b85de
RedonlineOfficial <dev@redonline.me>
2026-04-17 07:52:41 -0700

    feat: add alejandra to user packages
    Add alejandra to user packages for user joshua.  This allows running
    the formatter outside of neovim.  Added this so I could format in match

--- modules/users/joshua/default.nix
+++ modules/users/joshua/default.nix
@@ -1,10 +1,11 @@
-{ self, inputs, ... }: let
-
+{
+  self,
+  inputs,
+  ...
+}: let
   userName = "joshua";
-
 in {
-
-  flake.nixosModules.${userName} = { pkgs, ... }: {
+  flake.nixosModules.${userName} = {pkgs, ...}: {
     imports = [
       self.nixosModules.metaTerminal
     ];
@@ -14,13 +15,14 @@ in {
     users.users.${userName} = {
       isNormalUser = true;
       description = userName;
-      extraGroups = [ "wheel" "networkmanager" ];
+      extraGroups = ["wheel" "networkmanager"];
       shell = pkgs.zsh;
       packages = with pkgs; [
         claude-code
-        age         # age-keygen for generating admin key
-        sops        # encrypt/decrypt secrets files
-        ssh-to-age  # convert SSH ed25519 public keys to age public keys
+        age # age-keygen for generating admin key
+        sops # encrypt/decrypt secrets files
+        ssh-to-age # convert SSH ed25519 public keys to age public keys
+        alejandra
       ];
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCnzbcr6a2th9Sj1rHfKO7yAFJB30AhJRx5D5AFmA5zehfYygG2bNu+s2fewCXSClNXf2d1BaBVcW5dQetb5BtIiCEXXSiLzLQMA8K1RmntrWubZrkMtaoT8K7rr6pDJQV5WHZ7PLdaDNuR+45gfANRumSj4aPtDcfPW/gHUoN5gh2eHVKVya7/8Jg7iLzjZDPMXVb5YLFYqM4mOf0GNQ1X1pl8LVtTuaCJxn9xyCbqOo6Msx9pKa6ZNs1zDQcJSSkXniDc77hPtYgcBpRSL04JYX7WYVSgubeiJdDsoEPtPceImLFcswHnGjPW1Pshz+yBwBn6jUWeo/GzAk2mih6Hfgq15pNWvANH8jqaXv6LBzQo8Ll8b6bFlB3kYNF2zDw+/gfzbq2uQpnBPe4sE4GiSPLkiHKjmE2rr2Lu4ewcGpzMLJyEV/gPVYKvzhOCr9/fhIoy5R+WX8tQV+tqBw9U+adQjsrwrdDgRzvvAmzmiLWzIh2yQkh1ZAEOUwYc+0O60O8TMIyjwP8yNMppGoIU3MY35f45au/KyOLPgBXaKgBQziHU3ZLWfoE1PyShGOHEzxrjnw1UNGmx3AK+Z8JUApBVLohG0Wd8s2e71la+0M20bkwp2q2HeoExHTaYDvkojFqJ8tm5oRU3WC+10W3zZZ09J2ogAJc7VVpQ0mGHTQ=="
@@ -63,5 +65,4 @@ in {
       };
     };
   };
-
 }
